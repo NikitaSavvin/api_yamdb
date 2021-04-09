7308e32d5d319a5ba3b07c63ee9769255936b708
Petr <superdytko@gmail.com>
2021-04-09 20:54:58 +0300

    permissions commit

--- yamdb/permissions.py
+++ yamdb/permissions.py
@@ -7,12 +7,8 @@ class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
             request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
-            and (
-                request.user.role == request.user.is_admin
-                or request.user.is_superuser
+            or request.user.is_authenticated and request.user.is_admin
             )
-        )
 
 
 class IsAuthorOrStaffOrReadOnly(permissions.BasePermission):
